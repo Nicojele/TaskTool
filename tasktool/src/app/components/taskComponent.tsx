ea8375a1c069fa8dfc8bc0745c84536d98fd86e6
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2023-10-17 14:43:05 +0200

    Create strcture od component

---
+++ tasktool/src/app/components/taskComponent.tsx
@@ -0,0 +1,32 @@
+import { Category } from "@prisma/client";
+import styles  from "./components.module.css";
+
+type taskComponentProps = {
+  description: string
+  category: Category
+}
+
+export default function TaskComponent(props: taskComponentProps) {
+  return (
+    <>
+      <div className={styles.ComponentBody}>
+          <div className={styles.TextConatiner}>
+            <text className={styles.DescriptionText}>
+              {props.description}
+            </text>
+            <text className={styles.CategoryText}>
+              {props.category}
+            </text>
+        </div>
+        <div className={styles.OptionButtonContainer}>
+          <button className={styles.FinishedButton}>
+            finished
+          </button>
+          <button className={styles.Cancelbutton}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
