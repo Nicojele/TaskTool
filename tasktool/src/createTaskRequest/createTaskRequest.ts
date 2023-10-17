985987a37a031da13005495f6882e8ec2ba15e80
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2023-10-17 14:02:48 +0200

    Fix typo in createTaskRequest

--- tasktool/src/createTaskRequest/createTaskRequest.ts
+++
@@ -1,9 +0,0 @@
-import { Category } from "@prisma/client";
-
-export interface createTaskRequest {
-  description: string,
-  createdAt: Date,
-  finishedAt: Date,
-  finsihed: boolean,
-  category: Category
-}
