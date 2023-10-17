47e5fc566e1c54350f9765c516c96f40d336d6b9
Daniel Heibach <danielheibach@MacBook-Pro-von-Daniel.local>
2023-10-17 10:46:05 +0200

    layout change, new buttons, raw version

--- tasktool/src/app/page.tsx
+++ tasktool/src/app/page.tsx
@@ -1,42 +1,51 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { toUnicode } from 'punycode'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+          {/* <div>
+            <input type="beschreibung" name="task"></input>
+          </div> */}
+          <div>
+            <button className={styles.button}>open</button>
+          </div>
+          <div>
+            <button className={styles.button}>closed</button>
+          </div>
+          <div>
+            <button className={styles.button}>Filter</button>
+          </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.description}>
+          <div>
+            <input type="beschreibung" name="task"></input>
+          </div>
+          {/* <div>
+            <button>✘</button>
+          </div> */}
+          <div className={styles.dropdown}>
+            {/* <button className={styles.dropbtn}>Projekt</button>
+            <div className={styles.dropdownContent}>
+              <a href="#">Projekt 1</a>
+              <a href="#">Projekt 2</a>                   Wird später benötigt!!!
+              <a href="#">Projekt 3</a>
+            </div> */}
+          </div>
+          <div className={styles.dropdown}>
+            <button className={styles.dropbtn}>Dringlichkeit</button>
+            <div className={styles.dropdownContent}>
+              <a href="#">wichtig & dringend</a>
+              <a href="#">wichtig & nicht dringend</a>
+              <a href="#">nicht wichtig & dringend</a>
+              <a href="#">nicht wichtig & nicht dringend</a>
+            </div>
+          </div>
+          <div>
+            <button>✓</button>
+          </div>
       </div>
 
       <div className={styles.grid}>
@@ -47,7 +56,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+          wichtig & dringend <span>-&gt;</span>
           </h2>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
@@ -59,7 +68,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+          wichtig & nicht dringend <span>-&gt;</span>
           </h2>
           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
         </a>
@@ -71,7 +80,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Templates <span>-&gt;</span>
+          nicht wichtig & dringend <span>-&gt;</span>
           </h2>
           <p>Explore the Next.js 13 playground.</p>
         </a>
@@ -83,7 +92,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Deploy <span>-&gt;</span>
+          nicht wichtig & nicht dringend <span>-&gt;</span>
           </h2>
           <p>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
