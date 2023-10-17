86b3f9cfa216a2585982c30e5190e53f31610a5f
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2023-10-17 14:02:29 +0200

    Move api folder

--- tasktool/src/app/api/task/route.ts
+++ tasktool/src/app/api/task/route.ts
@@ -1,21 +1,30 @@
 
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from '../../../lib/prisma';
+import prisma from '../../../../lib/prisma';
 
 export async function POST(
   req: NextRequest
 ) {
   const data = await req.json()
+  console.log(data);
 
   const res = await prisma.task.create({
     data: {
       category: data.category,
-      createtAt: data.createtAt,
+      createtAt: data.createdAt,
       description: data.description,
-      finished: data.finished,
       finishedAt: data.finishedAt,
+      finished: data.finished,
     }
   })
+
+  return NextResponse.json({ res });
+}
+
+export async function GET(
+  req: NextRequest
+) {
+  const res = await prisma.task.findMany();
 
   return NextResponse.json({ res });
 }
