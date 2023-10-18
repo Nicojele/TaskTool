5e2b3b53ff675989b40b0e9fb035e0c8c305b1f6
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2023-10-18 14:17:17 +0200

    Add TestPage

--- tasktool/src/app/test/page.tsx
+++ tasktool/src/app/test/page.tsx
@@ -7,27 +7,9 @@ export default function test(): JSX.Element {
 
   return (
     <>
-      <button onClick={async () => createTask()}>Create Task</button>
       <button onClick={async () => logTasks()}>Log Tasks</button>
     </>
   )
-}
-
-async function createTask() {
-  const data: createTaskRequest = { category: Category.Dringend, createdAt: new Date(), description: 'Its a test to see if i am able to create a Task with a button press', finished: false, finishedAt: new Date() }
-  
-  await fetch("api/task", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {'Content-Type': 'application/json'},
-  })
-    .then(async response => {
-
-      return response.json();
-    })
-      .catch(error => {
-        console.error(error);
-      })
 }
 
 async function logTasks() {
