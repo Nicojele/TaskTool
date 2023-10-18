c3a0482a310431aae0346007ab9187dc313f09e1
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2023-10-18 14:16:41 +0200

    Add undefined as type for finished at

--- tasktool/src/app/createTaskRequest/createTaskRequest.ts
+++ tasktool/src/app/createTaskRequest/createTaskRequest.ts
@@ -3,7 +3,7 @@ import { Category } from "@prisma/client";
 export interface createTaskRequest {
   description: string,
   createdAt: Date,
-  finishedAt?: Date,
+  finishedAt?: Date | undefined,
   finished: boolean,
   category: Category
 }
