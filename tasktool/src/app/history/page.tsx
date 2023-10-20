ffb726c2e194298a6e62b8bded5f001505cde95f
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2023-10-20 13:26:12 +0200

    Add History div

--- tasktool/src/app/history/page.tsx
+++ tasktool/src/app/history/page.tsx
@@ -1,23 +1,11 @@
-'use client';
-
-import Link from "next/link";
-import styles from "../page.module.css";
+import styles from "./history.module.css";
 
 export default function showOrderView(): JSX.Element {
 
   return (
     <>
-       <div className={styles.description}>
-          <div>
-            <button className={styles.button}>open</button>
-          </div>
-          <div>
-            <button className={styles.button}><Link href="./history">closed</Link></button>
-          </div>
-      </div>
       <div className={styles.historyContainer}>
-        <div className={styles.hirstory}>
-
+        <div className={styles.content}>
         </div>
       </div>
     </>
