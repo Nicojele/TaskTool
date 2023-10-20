a2cd4473bd52225335c0cbfa18e8199c0f6096ee
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2023-10-20 15:59:40 +0200

    use prisma client insetad of fetch route

--- tasktool/src/app/history/page.tsx
+++ tasktool/src/app/history/page.tsx
@@ -1,11 +1,30 @@
+import { Task } from "@prisma/client";
 import styles from "./history.module.css";
+import { useEffect, useState } from "react";
+import TaskComponent from "../components/taskComponent";
+import prisma from "../../../lib/prisma";
 
-export default function showOrderView(): JSX.Element {
+export default async function showOrderView(): Promise<JSX.Element> {
+
+  const tasks = await prisma.task.findMany();
+  const finishedTasks: Array<Task> = [];
+  for (let index = 0; index < tasks.length; index++) {
+    const task = tasks[index];
+    if (task.finished) {
+      finishedTasks.push(task);
+    }  
+  }
+  console.log(finishedTasks)
 
   return (
     <>
-      <div className={styles.historyContainer}>
-        <div className={styles.content}>
+      <div className={styles.body}>
+        <div className={styles.historyContainer}>
+          <div className={styles.content}>
+            {finishedTasks.map((task) => (
+              <TaskComponent category={task.category} description={task.description}></TaskComponent>
+              ))}
+          </div>
         </div>
       </div>
     </>
