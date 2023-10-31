3d98727c3bd868e345811588da9ac1c166098c8b
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2023-10-31 09:25:07 +0100

    use finished tasks to show

--- tasktool/src/app/history/page.tsx
+++ tasktool/src/app/history/page.tsx
@@ -1,29 +1,45 @@
-import { Task } from "@prisma/client";
+'use client'
+
 import styles from "./history.module.css";
 import { useEffect, useState } from "react";
 import TaskComponent from "../components/taskComponent";
-import prisma from "../../../lib/prisma";
+import { Task } from "@prisma/client";
 
-export default async function showOrderView(): Promise<JSX.Element> {
+interface TaskToolState {
+  tasks: Task[]
+}
 
-  const tasks = await prisma.task.findMany();
-  const finishedTasks: Array<Task> = [];
-  for (let index = 0; index < tasks.length; index++) {
-    const task = tasks[index];
-    if (task.finished) {
-      finishedTasks.push(task);
-    }  
-  }
-  console.log(finishedTasks)
+export default function showOrderView(): JSX.Element {
+  const [state, setState] = useState<TaskToolState>({
+    tasks: []
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await fetch("api/task", {
+        method: "GET",
+        headers: {"Content-Type" : "application/json"}
+      })
+      const res = await req.json()
+
+      setState({ tasks: res.res })
+    }
+
+    fetchData();
+  }, [])
+
+  console.log(state.tasks);
 
   return (
     <>
       <div className={styles.body}>
         <div className={styles.historyContainer}>
           <div className={styles.content}>
-            {finishedTasks.map((task) => (
-              <TaskComponent task={task}></TaskComponent>
-              ))}
+            {state.tasks.map((task) => (
+              task.finished ? (
+                <TaskComponent task={task}></TaskComponent>
+              ) : null
+            ))}
           </div>
         </div>
       </div>
