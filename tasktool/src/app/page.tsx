4a6b93e93cfdd11f2f05a7974a1cc0dc6f4ab421
Nico Jeleniewski <nico.jeleniewski@5minds.de>
2023-12-12 09:01:31 +0100

    Add From to page

--- tasktool/src/app/page.tsx
+++ tasktool/src/app/page.tsx
@@ -5,25 +5,24 @@ import { Category, Task } from '@prisma/client'
 import { createTaskRequest } from './createTaskRequest/createTaskRequest'
 import { useEffect, useState } from 'react'
 import TaskComponent from './components/taskComponent'
+import CreateTaskForm from './components/createTaskForm'
 
 interface TaskToolState {
   category: Category | undefined
   tasks: Task[]
-  buttonText: string
 }
 
 export default function Home() {
   const [state, setState] = useState<TaskToolState>({
     category: undefined,
     tasks: [],
-    buttonText: "Dringlichkeit",
   });
 
   useEffect(() => {
     async function fetchData() {
       const req = await fetch("api/task", {
         method: "GET",
-        headers: {"Content-Type" : "application/json"}
+        headers: { "Content-Type": "application/json" }
       })
       const res = await req.json()
 
@@ -36,45 +35,18 @@ export default function Home() {
         }
       }
       
-      setState({ category: state.category, tasks: tasks, buttonText: "Dringlichkeit" })
+      setState({ category: state.category, tasks: tasks })
     }
 
     fetchData();
   }, [])
 
-  function Dropdown() {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-    };
-
-    const handleItemClick = (category: Category, buttonText: string) => {
-      setState({ category: category, buttonText: buttonText, tasks: state.tasks })
-      setIsOpen(false);
-    };
-
-    return (
-      <div>
-        <button className={styles.dropbtn} onClick={toggleDropdown}>{state.buttonText}</button>
-          {isOpen && (
-            <div className={styles.dropdownContent}>
-              <a onClick={() => handleItemClick(Category.WichtigDringend, "Wichtig & dringend")}>wichtig & dringend</a>
-              <a onClick={() => handleItemClick(Category.Wichtig,"Wichtig & nicht dringend")}>wichtig & nicht dringend</a>
-              <a onClick={() => handleItemClick(Category.Dringend, "Dringend & nicht wichtig")}>nicht wichtig & dringend</a>
-              <a onClick={() => handleItemClick(Category.Unwichtig, "nicht dringend & nicht wichtig")}>nicht wichtig & nicht dringend</a>
-            </div>
-          )}
-        </div>
-    );
-  }
-
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-          <div>
-            <input className={styles.textField} id='description' type="beschreibung" name="task"></input>
-          </div>
+        <div>
+          <CreateTaskForm />
+        </div>
           {/* <div className={styles.dropdown}>
             <button className={styles.dropbtn}>Projekt</button>
             <div className={styles.dropdownContent}>
@@ -83,12 +55,6 @@ export default function Home() {
               <a href="#">Projekt 3</a>
             </div>
           </div> */}
-          <div className={styles.dropdown}>
-            <Dropdown></Dropdown>
-          </div>
-          <div>
-            <button className={styles.button} onClick={async () => { if (state.category !== undefined) { createTask(state.category) }}}>✓</button>
-          </div>
       </div>
 
       <div className={styles.grid}>
@@ -160,32 +126,33 @@ export default function Home() {
   )
 }
 
-async function createTask(category: Category) {
-  const description = (document.getElementById("description") as HTMLInputElement).value
+// async function createTask(category: Category) {
+//   const description = (document.getElementById("description") as HTMLInputElement).value;
 
-  const data: createTaskRequest = { category: category, createdAt: new Date(), description: description, finished: false, finishedAt: undefined, };
+//   const data: createTaskRequest = { category: category, createdAt: new Date(), description: description, finished: false, finishedAt: undefined, };
   
-  (document.getElementById("description") as HTMLInputElement).value = "";
+//   (document.getElementById("description") as HTMLInputElement).value = "";
 
-  await fetch("api/task", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {'Content-Type': 'application/json'},
-  })
-    .then(async response => {
+//   await fetch("api/task", {
+//     method: "POST",
+//     body: JSON.stringify(data),
+//     headers: {'Content-Type': 'application/json'},
+//   })
+//     .then(async response => {
 
-      return response.json();
-    })
-      .catch(error => {
-        console.error(error);
-      })
-  
-  location.reload();
-}
+//       return response.json();
+//     })
+//     .catch(error => {
+//       console.error(error);
+//     })
 
-/* Zeichen zum kopieren:
-
-✘
+//     location.reload();
+//   }
+    
+    
+    /* Zeichen zum kopieren:
+    
+    ✘
 ✓
 
 */
